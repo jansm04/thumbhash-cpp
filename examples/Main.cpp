aea1335caca38bd0a3faa3c9a119d90fe6527200
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-03-22 12:15:12 -0400

    added use case

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -7,7 +7,15 @@ using namespace std;
 
 int main(void) {
     Image input;
-	input.ReadFromFile("images-original/malachi-60x87.png");
-    
+	bool is_loaded = input.ReadFromFile("examples/images-original/flower.png");
+	if (!is_loaded) return 0;
+	ThumbHash th;
+	uint8_t *hash = th.RGBAToThumbHash(input);
+	if (hash) {
+		Image output = th.ThumbHashToRGBA(hash);
+		output.WriteToFile("examples/images-output/flower.png");    
+	} else {
+		cout << "Image size too big." << endl;
+	}
 	return 0;
 }
